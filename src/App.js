32303f57f9dda780d9f274c1d367b4fd4f7f7ba2
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-19 19:36:42 +0100

    added Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import "./bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./pages/HomeScreen";
+import MatchScreen from "./pages/MatchScreen";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<HomeScreen />}></Route>
+        <Route path="/match/:id" element={<MatchScreen />}></Route>
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+  );
 }
 
 export default App;

d82bd43a307ab780a422d0439026ea3bed816cab
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-19 18:03:58 +0100

    added Icon for Match Page

---
+++ src/components/Icons/PlayerOffIcon.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PlayerOffIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path
+        fill="#E74C3C"
+        d="M5,6.41L6.41,5L17,15.59V9H19V19H9V17H15.59L5,6.41Z"
+      />
+    </svg>
+  );
+};
+
+export default PlayerOffIcon;
---
+++ src/components/Icons/PlayerOnIcon.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PlayerOnIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path
+        fill="#18BC9C"
+        d="M19,17.59L17.59,19L7,8.41V15H5V5H15V7H8.41L19,17.59Z"
+      />
+    </svg>
+  );
+};
+
+export default PlayerOnIcon;

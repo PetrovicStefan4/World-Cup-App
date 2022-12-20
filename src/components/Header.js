eced86913e0843872471c9ac180c01c2b1083b11
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-20 18:20:35 +0100

    added simple nav with react bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
   return (
     <header className="bg-danger position-sticky top-0 start-0 end-0 z-index-10">
-      <nav className="container d-flex justify-content-between align-items-center py-2">
+      <Navbar bg="danger" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Qatar 2022</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/matches">Matches</Nav.Link>
+              <Nav.Link href="/teams">Teams</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* <nav className="container d-flex justify-content-between align-items-center py-2">
         <div className="nav-logo text-white">
           <Link to={"/"}>
             <span className="text-white">Qatar 2022</span>
@@ -27,7 +41,7 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
     </header>
   );
 };

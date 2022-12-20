6e492958d45db01e92d105e7b1116ed090e0dc5d
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-20 17:58:52 +0100

    added new Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./pages/HomeScreen";
 import MatchesScreen from "./pages/MatchesScreen";
 import MatchScreen from "./pages/MatchScreen";
 import TeamsScreen from "./pages/TeamsScreen";
@@ -13,7 +14,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<MatchesScreen />}></Route>
+        <Route path="/" element={<HomeScreen />}></Route>
         <Route path="/matches" element={<MatchesScreen />}></Route>
         <Route path="/match/:id" element={<MatchScreen />}></Route>
         <Route path="/teams" element={<TeamsScreen />}></Route>

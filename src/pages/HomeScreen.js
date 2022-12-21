0e7dacf339e966c1648c85c7a72671d9ddb875b2
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-21 16:13:12 +0100

    edited Hero on Homescreen

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -13,16 +13,20 @@ const HomeScreen = () => {
       >
         <div className="position-absolute top-0 bottom-0 start-0 end-0 z-index-2">
           <div className="h-100 container">
-            <div className="h-100 d-flex justify-content-center allign-items-center flex-column">
-              <h1 className="text-white mb-5">World Cup Qatar 2022</h1>
-              <h4 className="text-white">
-                Everything you wants to about World Cup.
-              </h4>
-              <h4 className="text-white mb-5">
-                Detailed statistics for matches and teams, and also the latest
-                news.
-              </h4>
-              <button className="btn btn-danger">Check latest news</button>
+            <div className="h-100 row d-flex justify-content-center allign-items-center flex-column">
+              <div className="col-12">
+                <h1 className="text-white mb-5">World Cup Qatar 2022</h1>
+                <h4 className="text-white">
+                  Everything you wants to about World Cup.
+                </h4>
+                <h4 className="text-white mb-5">
+                  Detailed statistics for matches and teams, and also the latest
+                  news.
+                </h4>
+                <button className="btn btn-success px-4">
+                  Check latest news
+                </button>
+              </div>
             </div>
           </div>
         </div>

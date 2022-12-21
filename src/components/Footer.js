45a98677d7ccf65756813f7e4a4e5d9534d9cf1b
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-21 17:40:18 +0100

    changing background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-danger">
+    <footer className="bg-success">
       <div className="container d-flex justify-content-center align-items-center py-2">
         <p className="text-white mb-0">Copyrights reserved 2022</p>
       </div>

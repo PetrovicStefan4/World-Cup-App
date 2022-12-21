7b72b042ac9cd83150c896045518c5592f88167f
PetrovicStefan4 <petrovicstefan0404@gmail.com>
2022-12-21 17:40:45 +0100

    chaning bg color and adding trophy icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import TrophyIcon from "./Icons/TrophyIcon";
 
 const Header = () => {
   return (
-    <header className="bg-danger position-sticky top-0 start-0 end-0 z-index-10">
-      <Navbar bg="danger" variant="dark" expand="lg" collapseOnSelect>
+    <header className="position-sticky top-0 start-0 end-0 z-index-10">
+      <Navbar
+        bg="success"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+        scrolling
+      >
         <Container>
-          <Navbar.Brand href="/">Qatar 2022</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <div className="d-flex justify-content-center align-items-center">
+              <span className="me-2">
+                <TrophyIcon />
+              </span>
+              <span className="fs-24 fw-bold">Qatar 2022</span>
+            </div>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/matches">Matches</Nav.Link>
-              <Nav.Link href="/teams">Teams</Nav.Link>
+              <Nav.Link href="/">
+                <span className="fw-bold fs-18">Home</span>
+              </Nav.Link>
+              <Nav.Link href="/matches">
+                <span className="fw-bold fs-18">Matches</span>
+              </Nav.Link>
+              <Nav.Link href="/teams">
+                <span className="fw-bold fs-18">Teams</span>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* <nav className="container d-flex justify-content-between align-items-center py-2">
-        <div className="nav-logo text-white">
-          <Link to={"/"}>
-            <span className="text-white">Qatar 2022</span>
-          </Link>
-        </div>
-        <ul className="d-flex justify-content-center align-items-center">
-          <li className="me-3">
-            <Link to={"/"}>
-              <span className="nav-link text-white">Home</span>
-            </Link>
-          </li>
-          <li className="me-3">
-            <Link to={"/matches"}>
-              <span className="nav-link text-white">Matches</span>
-            </Link>
-          </li>
-          <li>
-            <Link to={"/teams"}>
-              <span className="nav-link text-white">Teams</span>
-            </Link>
-          </li>
-        </ul>
-      </nav> */}
     </header>
   );
 };
